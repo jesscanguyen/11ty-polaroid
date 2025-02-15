df7e85dda4772de7c6ff6545a575f5693c309cc1
jesscanguyen <126618898+jesscanguyen@users.noreply.github.com>
2025-02-15 19:58:52 +0000

    started over, made classes into partials and added JSON data file

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,4 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 
-eleventyConfig.addShortcode("test", function(bop) {
-  return `
-    <p>test ${bop}</p>
-`;
-});
-
 };
