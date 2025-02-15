fcbbc066f322a6af538890a0aebe99e6a7bf23bb
jesscanguyen <126618898+jesscanguyen@users.noreply.github.com>
2025-02-15 08:33:06 +0000

    sections created

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+eleventyConfig.addShortcode("test", function(bop) {
+  return `
+    <p>test ${bop}</p>
+`;
+});
+
 };
